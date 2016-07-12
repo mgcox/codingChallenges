66242373f46a314206d68776a49d9c4330012810
Matthew <mgarrettcox@gmail.com>
2016-07-12 08:57:11 -0600

    Initial Commit

---
+++ numbersToBinary.py
@@ -0,0 +1,14 @@
+t = int(raw_input())
+
+for num in range(0,t):
+    val = int(raw_input())
+    for i in range(1,val+1):
+        print (bin(i)[2:]),
+    print '\n'#code
+t = int(raw_input())
+
+for num in range(0,t):
+    val = int(raw_input())
+    for i in range(1,val+1):
+        print (bin(i)[2:]),
+    print '\n'
